5dbf90fda1f916c04f2a6c144107a5669325a504
ColoreFreddo <105849444+ColoreFreddo@users.noreply.github.com>
2022-05-19 11:47:16 +0200

    Add files via upload

---
+++ Ping_viola/Ping_viola/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Ping_viola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAvvia = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAvvia
+            // 
+            this.btnAvvia.Location = new System.Drawing.Point(913, 484);
+            this.btnAvvia.Name = "btnAvvia";
+            this.btnAvvia.Size = new System.Drawing.Size(75, 23);
+            this.btnAvvia.TabIndex = 0;
+            this.btnAvvia.Text = "avvia";
+            this.btnAvvia.UseVisualStyleBackColor = true;
+            this.btnAvvia.Click += new System.EventHandler(this.btnAvvia_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1049, 588);
+            this.Controls.Add(this.btnAvvia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAvvia;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ping_viola/Ping_viola/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace Ping_viola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TextBox[] txtms = new TextBox[10];
+        TextBox[] txtIP = new TextBox[10];
+        TextBox[] txtStatus = new TextBox[10];
+        private void btnCarica_Click(object sender, EventArgs e)
+        {
+
+        }
+        string nomefile="viola.si";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader File = new StreamReader(nomefile);
+            string trmm=File.ReadToEnd();
+            string[] campi = trmm.Split('|');
+            //generazione tabelle
+            for (int i = 0; i < 10; i++)
+            {
+                txtIP[i] = new TextBox();
+                txtIP[i].Top = (int)(i / 2) * 50 + 80;
+                txtIP[i].Left = i % 2 * 500 + 100;
+                txtIP[i].Text = campi[i].ToString();
+                this.Controls.Add(txtIP[i]);
+                txtStatus[i] = new TextBox();
+                txtStatus[i].Top = (int)(i / 2) * 50 + 80;
+                txtStatus[i].Left = i % 2 * 500 + 100 + 100;
+                
+                this.Controls.Add(txtStatus[i]);
+                txtms[i] = new TextBox();
+                txtms[i].Top = (int)(i / 2) * 50 + 80;
+                txtms[i].Left = i % 2 * 500 + 200 + 100;
+               
+                this.Controls.Add(txtms[i]);
+            }
+        }
+
+        private void btnAvvia_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string Indirizzo = txtIP[i].Text;
+
+                Ping pinger = new Ping();
+                PingReply reply = pinger.Send(Indirizzo);
+                string status = reply.Status.ToString();
+                string millisec = reply.RoundtripTime.ToString();
+                txtStatus[i].Text = status;
+                txtms[i].Text = millisec;
+                this.Cursor = Cursors.Arrow;
+                if (txtStatus[i].Text== "Success")
+                {
+                    txtStatus[i].BackColor = Color.Green;
+                }
+                else
+                {
+                    txtStatus[i].BackColor = Color.Red;
+                }
+            }
+        }
+    }
+}
